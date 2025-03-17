380609dfb1c042023b5e0de17b682a891a83b4bc
Felipe Caroca <felipecaroca24@gmail.com>
2025-03-17 16:17:52 -0300

    Correccion url stripecontroller

--- backend/src/controllers/stripeController.js
+++ backend/src/controllers/stripeController.js
@@ -33,8 +33,8 @@ const createCheckoutSession = async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: "https://fitmarket-frontend.onrender.com/success",
+      cancel_url: "https://fitmarket-frontend.onrender.com/cancel",
       metadata: {
         userId: req.user.id,
         cartItems: JSON.stringify(
