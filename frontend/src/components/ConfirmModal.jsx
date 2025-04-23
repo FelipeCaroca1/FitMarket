cf883e1c9359694faae1e24e0954295af2582581
Felipe Caroca <felipecaroca24@gmail.com>
2025-04-23 15:11:49 -0400

    Correccion UX botones en modal de confirmacion

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import Button from "./Button"; 
+import Button from "./Button";
 
 const ConfirmModal = ({ isOpen, title, message, onConfirm, onCancel }) => {
   if (!isOpen) return null;
@@ -10,10 +10,16 @@ const ConfirmModal = ({ isOpen, title, message, onConfirm, onCancel }) => {
         <h3 className="text-xl font-semibold mb-4">{title}</h3>
         <p className="mb-6 text-gray-300">{message}</p>
         <div className="flex justify-end space-x-4">
-          <Button size="medium" onClick={onCancel} >
+          <Button
+            size="medium"
+            className="!border-green-700 !text-white hover:!bg-green-900"
+            onClick={onConfirm}
+          >
+            Confirmar
+          </Button>
+          <Button size="medium" onClick={onCancel}>
             Cancelar
           </Button>
-          <Button size="medium" className="!border-green-700 !text-white hover:!bg-green-900 " onClick={onConfirm}>Confirmar</Button>
         </div>
       </div>
     </div>
