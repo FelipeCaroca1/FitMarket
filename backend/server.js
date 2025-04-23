fdd65bea3d2c37281de5f75d734d188ac484edb1
Felipe Caroca <felipecaroca24@gmail.com>
2025-04-23 15:25:22 -0400

    Correcion path de img

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "../backend/public/img")));
+app.use("/images", express.static(path.join(__dirname, "public", "img")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
